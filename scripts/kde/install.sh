4d6a08c3361fc5b35c1ac5d5dbcf49549a30a858
Aleksey Midenkov <midenok@gmail.com>
2013-11-07 13:49:11 +0400

    Path fix

--- scripts/kde/install.sh
+++ scripts/kde/install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-make install prefix=$HOME/src/kde/opt/usr sysconfdir=$HOME/src/kde/opt/etc INSTALL="$HOME/bin/alternative-install/install -p -c"
+make install prefix=$HOME/src/kde/opt/usr sysconfdir=$HOME/src/kde/opt/etc INSTALL="$HOME/bin/linked-install/install -p -c"
