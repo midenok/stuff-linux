e18eb163c67cb085d3b5fa57037c5b100c6d28d7
Aleksey Midenkov <midenok@gmail.com>
2014-06-10 15:13:22 +0400

    linked_install CMake: multiple files fix

--- scripts/devel/linked-install/CMake/linked_install.cmake
+++ scripts/devel/linked-install/CMake/linked_install.cmake
@@ -3,6 +3,10 @@ macro(pop list out)
     list(REMOVE_AT ${list} 0)
 endmacro()
 
+macro(push list in)
+    list(APPEND ${list} ${in}) 
+endmacro()
+
 macro(join list delim out)
     set(sep "")
     foreach(arg ${${list}})
@@ -14,30 +18,59 @@ endmacro()
 macro(symlink src dst)
     get_filename_component(file "${src}" NAME)
     set(dst_file "${dst}/${file}")
+    if(NOT IS_DIRECTORY "${dst}")
+        execute_process(
+            COMMAND mkdir -p "${dst}"
+            RESULT_VARIABLE rc)
+        if(rc)
+            message(FATAL_ERROR "Failed mkdir -p ${dst}")
+        endif()
+    endif()
     execute_process(
         COMMAND cmake -E create_symlink "${src}" "${dst_file}"
         RESULT_VARIABLE rc)
     if(rc)
         message(FATAL_ERROR "create_symlink failed! old:${src} new:${dst_file}")
+    else()
+        message("-- Symlinking: ${dst_file}")
     endif()
 endmacro()
 
 function(FILE CMD)
     if(CMD STREQUAL INSTALL)
         set(args ${ARGN})
-        join(args " " trace)
-        message(${trace})
+        set(cmd FILES)
+        # join(args " " trace)
+        # message(${trace})
         while (args)
             pop(args arg)
             if(arg STREQUAL DESTINATION)
+                set(cmd ${arg})
                 pop(args dest)
             elseif(arg STREQUAL FILES)
-                # FIXME: multiple files?
+                set(cmd ${arg})
                 pop(args files)
+            elseif(arg STREQUAL FILE_PERMISSIONS OR
+                    arg STREQUAL DIRECTORY_PERMISSIONS OR
+                    arg STREQUAL NO_SOURCE_PERMISSIONS OR
+                    arg STREQUAL USE_SOURCE_PERMISSIONS OR
+                    arg STREQUAL FILES_MATCHING OR
+                    arg STREQUAL PATTERN OR
+                    arg STREQUAL REGEX OR
+                    arg STREQUAL EXCLUDE OR
+                    arg STREQUAL PERMISSIONS OR
+                    arg STREQUAL TYPE)
+                set(cmd ${arg})
+            else()
+                if(cmd STREQUAL FILES)
+                    push(files "${arg}")
+                endif()
             endif()
         endwhile()
-        message("dest: " ${dest} "; files: " ${files})
-        symlink(${files} ${dest})
+        # message("dest: " ${dest} "; files: " ${files})
+        foreach(file ${files})
+            symlink(${file} ${dest})
+        endforeach()
     else()
         _FILE(${ARGV})
     endif()
