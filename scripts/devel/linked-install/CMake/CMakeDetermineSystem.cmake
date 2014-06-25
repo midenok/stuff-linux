2769b8c0bb92bb4e37e043897e531a89d4abcf92
Aleksey Midenkov <midenok@gmail.com>
2014-06-25 12:07:08 +0400

    linked-install: moved to separate repo ln-install

--- scripts/devel/linked-install/CMake/CMakeDetermineSystem.cmake
+++
@@ -1,5 +0,0 @@
-include(${CMAKE_ROOT}/Modules/CMakeDetermineSystem.cmake)
-file(APPEND
-    ${CMAKE_PLATFORM_INFO_DIR}/CMakeSystem.cmake
-    "install(CODE \"SET(CMAKE_MODULE_PATH \\\"${CMAKE_MODULE_PATH}\\\")
-        INCLUDE(linked_install)\")")
