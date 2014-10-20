60981da8bde807598d8ca7c60fb53e34f92b9eb1
midenok <midenok@gmail.com>
2014-10-20 15:42:19 +0400

    dd wrapper to show progress

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -50,3 +50,33 @@ commit()
     done
     rm $batch
 )}
+
+dd()
+{
+    local dd=$(which dd); [ "$dd" ] || {
+        echo "'dd' is not installed!" >&2
+        return 1
+    }
+
+    local pv=$(which pv); [ "$pv" ] || {
+        echo "'pv' is not installed!" >&2
+        "$dd" "$@"
+        return $?
+    }
+    
+    local arg arg2 infile
+    local -a args
+    for arg in "$@"
+    do
+        arg2=${arg#if=}
+        if [ "$arg2" != "$arg" ]
+        then
+            infile=$arg2
+        else
+            args[${#args[@]}]=$arg
+        fi
+    done
+    
+    "$pv" -tpreb "$infile" | "$dd" "${args[@]}"
+}
+
